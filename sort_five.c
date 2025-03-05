4c3b390ecf4de95c3a980ca71742d69d6cad36ee
Chiara Kappe <chiarakappe@Chiaras-MacBook-Pro-2.local>
2025-03-05 10:43:42 +0100

    sketch for algo sorting five numbers

---
+++ sort_five.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_five.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 20:46:05 by chiarakappe       #+#    #+#             */
+/*   Updated: 2025/03/04 19:21:28 by chiarakappe      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// stack_a and stack_b
+
+
+
+// find the two smallest numbers in stack_a
+
+// push two numbers to stack_b, use sort three to sort the rest
+// pb, pb, sort_three(stack_a)
+// if stack_b[0] > stack_[0] 
+//		sb;

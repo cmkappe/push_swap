6c889909a58349b21d87430a8b77c214e3069bad
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-03-05 16:51:51 +0100

    custom solution for five numbers, reusing logic from chunk_sort

--- sort_five.c
+++ sort_five.c
@@ -3,22 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   sort_five.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 20:46:05 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/03/04 19:21:28 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/05 16:50:14 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-// stack_a and stack_b
+int	find_index_of_min(t_stack *stack)
+{
+	int	i;
+	int	min;
+	int	index;
 
+	i = 0;
+	min = stack->arr[0];
+	index = 0;
+	while (i < stack->size)
+	{
+		if (stack->arr[i] < min)
+		{
+			min = stack->arr[i];
+			index = i;
+		}
+		i++;
+	}
+	return (index);
+}
 
+void	sort_five(t_stack *stack_a, t_stack *stack_b)
+{
+	int	index;
+	int	value;
+	int	index_a;
 
-// find the two smallest numbers in stack_a
-
-// push two numbers to stack_b, use sort three to sort the rest
-// pb, pb, sort_three(stack_a)
-// if stack_b[0] > stack_[0] 
-//		sb;
+	while (stack_a->size > 3)
+	{
+		index = find_index_of_min(stack_a);
+		bring_to_top(stack_a, index, 'a');
+		pb(stack_a, stack_b);
+	}
+	sort_three(stack_a);
+	while (stack_b->size > 0)
+	{
+		value = stack_b->arr[0];
+		index_a = find_best_spot(stack_a, value);
+		bring_to_top(stack_a, index_a, 'a');
+		pa(stack_a, stack_b);
+	}
+}

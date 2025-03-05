6a1628017c0431d9fc6dcb6f4407bc82d04b0048
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-03-05 16:14:04 +0100

    fitted to use struct & fixed norminette errors

--- map_to_ranks.c
+++ map_to_ranks.c
@@ -3,128 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   map_to_ranks.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 14:56:05 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/02/21 15:56:13 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/05 16:13:10 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-/*
- * map_to_ranks: Given an array 'arr' of length 'size', 
- * returns a new array where each element is replaced by its rank
- * The smallest element gets rank 0, the next gets 1, ...;
- *
- * uses bubble sort to sort a copy of the arry
- */
-
-/* int	*map_to_ranks(int *arr, int size)
-{
-	int	*copy;
-	int	i;
-	int	swapped;
-	int	temp;
-	int	*mapped;
-	int	j;
-
-	// copy of the original array
-	if (!(copy = (int *)malloc(sizeof(int) * size)))
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		copy[i] = arr[i];
-		i++;
-	}
-
-	// sort the copy with bubble sort
-	
-	swapped = 1;
-	while (swapped)
-	{
-		swapped = 0;
-		i = 0;
-		while (i < size - 1)
-		{
-			if (copy[i] > copy[i + 1])
-			{
-				temp = copy[i];
-				copy[i] = copy[i + 1];
-				copy[i + 1] = temp;
-				swapped = 1;
-			}
-			i++;
-		}
-	}
- 	// Allocate array for mapped values
-	mapped = (int *)malloc(sizeof(int) * size);
-	if (!mapped)
-	{
-		free(copy);
-		return (NULL);
-	}
-
-
-	// for each element in the original array, find its rank in the sorted copy
-	i = 0;
-	while (i < size)
-	{
-		j = 0;
-		while (j < size)
-		{
-			if (arr[i] == copy[j])
-			{
-				mapped[i] = j;
-				break;
-			}
-			j++;
-		}
-		i++;
-	}
-	free(copy);
-	return (mapped);
-} */
-
-
-
-
-int	*copy_array(int *arr, int size)
+int	*copy_array(t_stack *arr)
 {
 	int	i;
 	int	*copy;
 
 	i = 0;
-	copy = (int *)malloc(sizeof(int) * size);
+	copy = (int *)malloc(sizeof(int) * arr->size);
 	if (!copy)
 		return (NULL);
-	while (i < size) // why not size - 1
+	while (i < arr->size)
 	{
-		copy[i] = arr[i];
+		copy[i] = arr->arr[i];
 		i++;
 	}
 	return (copy);
 }
 
-void	bubble_sort(int *arr, int size)
+void	bubble_sort(t_stack *arr)
 {
 	int	i;
 	int	swapped;
 	int	temp;
-	
+
 	swapped = 1;
 	while (swapped)
 	{
 		i = 0;
 		swapped = 0;
-		while (i < size - 1)
+		while (i < arr->size - 1)
 		{
-			if (arr[i] > arr[i + 1])
+			if (arr->arr[i] > arr->arr[i + 1])
 			{
-				temp = arr[i];
-				arr[i] = arr[i + 1];
-				arr[i + 1] = temp;
+				temp = arr->arr[i];
+				arr->arr[i] = arr->arr[i + 1];
+				arr->arr[i + 1] = temp;
 				swapped = 1;
 			}
 			i++;
@@ -132,21 +54,21 @@ void	bubble_sort(int *arr, int size)
 	}
 }
 
-void	fill_mapping(int *arr, int *copy, int *mapped, int size)
+void	fill_mapping(t_stack *arr, int *copy, int *mapped)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while (i < size)
+	while (i < arr->size)
 	{
 		j = 0;
-		while (j < size)
+		while (j < arr->size)
 		{
-			if (arr[i] == copy[j])
+			if (arr->arr[i] == copy[j])
 			{
 				mapped[i] = j;
-				break;
+				break ;
 			}
 			j++;
 		}
@@ -154,22 +76,22 @@ void	fill_mapping(int *arr, int *copy, int *mapped, int size)
 	}
 }
 
-int	*map_to_ranks(int *arr, int size)
+int	*map_to_ranks(t_stack *arr)
 {
 	int	*copy;
 	int	*mapped;
 
-	copy = copy_array(arr, size);
+	copy = copy_array(arr);
 	if (!copy)
 		return (NULL);
-	bubble_sort(copy, size);
-	mapped = (int *)malloc(sizeof(int) * size);
+	bubble_sort(copy);
+	mapped = (int *)malloc(sizeof(int) * arr->size);
 	if (!mapped)
 	{
 		free(copy);
 		return (NULL);
 	}
-	fill_mapping(arr, copy, mapped, size);
+	fill_mapping(arr, copy, mapped);
 	free(copy);
 	return (mapped);
 }

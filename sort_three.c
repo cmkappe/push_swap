4f4dd764ebcaee324174a26e5bbf6c22447ae271
Chiara Marie Kappe <ckappe@2-G-6.42heilbronn.de>
2025-03-05 15:38:56 +0100

    reduced line count and fixed norminette errors

--- sort_three.c
+++ sort_three.c
@@ -3,97 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   sort_three.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
+/*   By: ckappe <ckappe@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 23:44:00 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/03/04 19:00:23 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/05 15:38:02 by ckappe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+// put check_if_sorted in front
+
+/* test cases:
+
+2 1 3
+3 2 1
+1 3 2
+2 3 1
+3 1 2 */
+
 void	sort_three(t_stack *stack)
 {
-	if (stack->arr[0] < stack->arr[1] && stack->arr[1] < stack->arr[2])
-		return;
-	else if (stack->arr[0] > stack->arr[1] && stack->arr[0] > stack->arr[2])
-	{
-		ra(stack);
-		return;
-	}
+	if (stack->arr[0] > stack->arr[1] && stack->arr[0] < stack->arr[2])
+		sa(stack);
 	else if (stack->arr[0] > stack->arr[1] && stack->arr[1] > stack->arr[2])
 	{
-		sa(stack);
-		return;
-	}
-	else if (stack->arr[2] > stack->arr[1] && stack->arr[2] > stack->arr[0])
-	{
 		ra(stack);
 		sa(stack);
-		return;
 	}
-	else if (stack->arr[1] > stack->arr[0] && stack->arr[1] > stack->arr[2])
+	else if (stack->arr[0] < stack->arr[1] && stack->arr[2] > stack->arr[0])
 	{
 		sa(stack);
 		ra(stack);
-		return;
 	}
-	else
-	{
+	else if (stack->arr[1] > stack->arr[0] && stack->arr[0] > stack->arr[2])
 		rra(stack);
-		return;
-	}
-}
-
-/* ra (rotate a): Shift up all elements of stack a by 1.
-The first element becomes the last one.
-
-rra (reverse rotate a): Shift down all elements of stack a by 1.
-The last element becomes the first one. */
-
-
-int main()
-{
-	t_stack	stack_a;
-	int	i;
-	
-	stack_a.size = 3;
-	stack_a.arr = (int *)malloc(sizeof(int) * stack_a.size);
-	if (!stack_a.arr)
-		return (1);
-	
-	i = 0;
-	while (i < stack_a.size)
-	{
-		if (i == 0)
-			stack_a.arr[i] = 3;
-		else if (i == 1)
-			stack_a.arr[i] = 1;
-		else
-			stack_a.arr[i] = 2;
-		i++;
-	}
-	
-	printf("stack_a before sorting ");
-	i = 0;
-	while (i < stack_a.size)
-	{
-		printf("%d", stack_a.arr[i]);
-		i++;
-	}
-	printf("\n");
-
-	sort_three(&stack_a);
-
-	printf("stack_a after sorting ");
-	i = 0;
-	while (i < stack_a.size)
-	{
-		printf("%d", stack_a.arr[i]);
-		i++;
-	}
-	printf("\n");
-
-	free(stack_a.arr);
-	return (0);
+	else if (stack->arr[0] > stack->arr[1] && stack->arr[1] < stack->arr[2])
+		ra(stack);
+	return ;
 }

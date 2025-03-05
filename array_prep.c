f757f11e4732af92baf965046af2eb2a4b600405
Chiara Kappe <chiarakappe@Chiaras-MacBook-Pro-2.local>
2025-03-05 10:55:46 +0100

    array_prep now gets the chunks for sorting

--- array_prep.c
+++ array_prep.c
@@ -6,124 +6,78 @@
 /*   By: chiarakappe <chiarakappe@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 00:25:27 by chiarakappe       #+#    #+#             */
-/*   Updated: 2025/02/17 01:56:05 by chiarakappe      ###   ########.fr       */
+/*   Updated: 2025/03/05 10:52:28 by chiarakappe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
 
-
-int	*map_to_ranks(int *arr, int size)
+int decide_chunks(t_stack size)
 {
-	int	*copy;
-	int	i;
-	int	swapped;
-	int	temp;
-	int	*mapped;
-	int	j;
+	if (size.size <= 100)
+		return (5);
+		//return 5;
+	else if (size.size <= 500)
+		return (10);
+	else
+		return (20);
+}
 
-	// copy of the original array
-	if (!(copy = (int *)malloc(sizeof(int) * size)))
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		copy[i] = arr[i];
-		i++;
-	}
-
-	// sort the copy with bubble sort
+void get_chunk_limits(int chunk_index, t_stack *stack, int *low, int *high)
+{
+	int	chunk_size;
+	int	amount_chunks;
 	
-	swapped = 1;
-	while (swapped)
-	{
-		swapped = 0;
-		i = 0;
-		while (i < size - 1)
-		{
-			if (copy[i] > copy[i + 1])
-			{
-				temp = copy[i];
-				copy[i] = copy[i + 1];
-				copy[i + 1] = temp;
-				swapped = 1;
-			}
-			i++;
-		}
-	}
- 	// Allocate array for mapped values
-	mapped = (int *)malloc(sizeof(int) * size);
-	if (!mapped)
-	{
-		free(copy);
-		return (NULL);
-	}
+	amount_chunks = decide_chunks(stack->arr);
+	chunk_size = stack->size / amount_chunks;
+	*low = chunk_index * chunk_size;
+	if (chunk_index == amount_chunks - 1)
+		*high = stack->size - 1;
+	else
+		*high = (chunk_index + 1) * chunk_size - 1;
+}
 
 
-	// for each element in the original array, find its rank in the sorted copy
+/* int	find_index_in_chunk(int *stack, int size, int low, int high)
+{
+	int	i;
+	int	index_top;
+	int	index_bottom;
+
 	i = 0;
+	index_top = -1;
 	while (i < size)
 	{
-		j = 0;
-		while (j < size)
+		if (stack[i] >= low && stack[i] <= high)
 		{
-			if (arr[i] == copy[j])
-			{
-				mapped[i] = j;
-				break;
-			}
-			j++;
+			index_top = i;
+			break;
 		}
 		i++;
 	}
-	free(copy);
-	return (mapped);
-}
+	i = size - 1;
+	index_bottom = -1;
+	while (i >= 0)
+	{
+		if (stack[i] >= low && stack[i] <= high)
+		{
+			index_bottom = i;
+			break;
+		}
+		i--;
+	}
+	if (index_top == -1)
+		return (-1);
+	if (index_bottom == -1)
+		return (index_top);
+	if (index_top <= (size - index_bottom))
+		return (index_top);
+	else
+		return (index_bottom);
+} */
 
 
-
-int main(void)
-{
-    // Test array
-    int arr[] = {42, 15, 73, -8, 23, -60, 4};
-    int size;
-    int i;
-    int *mapped;
-
-    size = sizeof(arr) / sizeof(arr[0]);
-
-    // Print the original array
-    printf("Original array:\n");
-    i = 0;
-    while (i < size)
-    {
-        printf("%d ", arr[i]);
-        i++;
-    }
-    printf("\n\n");
-
-    // Map the original array to ranks
-    mapped = map_to_ranks(arr, size);
-    if (!mapped)
-    {
-        printf("Error mapping ranks.\n");
-        return (1);
-    }
-
-    // Print the mapped array
-    printf("Mapped (ranked) array:\n");
-    i = 0;
-    while (i < size)
-    {
-        printf("%d ", mapped[i]);
-        i++;
-    }
-    printf("\n\n");
-
-    free(mapped);
-    return (0);
-}
 
 
 
